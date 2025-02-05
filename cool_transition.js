cb8e378c52b5a6e653b4010f817d7b2484f841d6
khang200923 <92591231+khang200923@users.noreply.github.com>
2025-02-05 22:40:08 +0700

    Refresh

--- cool_transition.js
+++ cool_transition.js
@@ -1,24 +1,3 @@
-const moreDef =
-`
-The word "<span class="cool-transition" cool-trans="more"></span>" is a common English word that has a variety of meanings and uses.
-It can be used as an <span class="cool-transition" cool-trans="adjective|  adverb |   noun  | pronoun "></span>.
-As an adjective, it is used to describe a greater amount, quantity, or degree of something.
-For example, "I need <span class="cool-transition" cool-trans="more"></span> time to finish my work."
-As an adverb, it is used to indicate an increase in degree or intensity.
-For example, "She spoke <span class="cool-transition" cool-trans="more"></span> softly."
-As a noun, it refers to a greater amount or additional quantity of something.
-For example, "I want <span class="cool-transition" cool-trans="more"></span> of that delicious cake."
-As a pronoun, it refers to a greater amount or additional quantity of something without specifying what that something is.
-For example, "I need <span class="cool-transition" cool-trans="more"></span>."
-Overall, "<span class="cool-transition" cool-trans="more"></span>" is a versatile word that is used extensively in English language to convey a wide range of meanings and ideas.
-`
-function giveMoreDef() {
-    const res = document.createElement('span');
-    yep = document.getElementById('holy-hell');
-    yep.parentNode.replaceChild(res, yep);
-    res.innerHTML = moreDef;
-}
-
 const transitionMid = `ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƒƠơƯưǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǺǻǼǽǾǿȘșȚț`
 
 function chooseTransMid() {
@@ -52,7 +31,7 @@ async function coolTransition() {
     const getTime = () => (performance.now() - startTime) / 1000
     setInterval(() => {
         let elements = document.getElementsByClassName('cool-transition');
-        for(let i=0;i<elements.length;i++){
+        for (let i=0;i<elements.length;i++) {
             let mode = (getTime()) % 2 < 1 ? 0 : (getTime()) % 2 - 1;
             let texts = elements[i].getAttribute("cool-trans").split('|');
             if (mode == 0) {
